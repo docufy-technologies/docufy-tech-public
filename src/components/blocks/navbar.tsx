ab2e205dd9c45f5d5d8fb554a0dce1bedf8d2eba
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-05-11 21:40:01 +0600

    feat: refactor navbar component to use NavTab for better structure and readability

--- src/components/blocks/navbar.tsx
+++ src/components/blocks/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link } from "@tanstack/react-router";
+import { Link, type LinkProps } from "@tanstack/react-router";
 import { motion } from "framer-motion";
 import type React from "react";
 import { useRef, useState } from "react";
@@ -25,54 +25,58 @@ function NavBar() {
       className="mx-auto flex w-fit rounded-full border-2 border-secondary bg-transparent backdrop-blur-xl z-100 fixed top-1 left-1/2 -translate-x-1/2 px-2 py-1 max-sm:p-1 gap-4 max-sm:gap-0"
       onMouseLeave={() => setPosition((pv) => ({ ...pv, opacity: 0 }))}
     >
-      <Tab setPosition={setPosition}>
-        <Link to="/">Home</Link>
-      </Tab>
-      <Tab setPosition={setPosition}>
-        <Link to="/about">About</Link>
-      </Tab>
-      <Tab setPosition={setPosition}>
-        <Link to="/solutions">Solutions</Link>
-      </Tab>
-      <Tab setPosition={setPosition}>
-        <Link to="/faq">FAQs</Link>
-      </Tab>
+      <NavTab setPosition={setPosition} to="/">
+        Home
+      </NavTab>
+      <NavTab setPosition={setPosition} to="/about">
+        About
+      </NavTab>
+      <NavTab setPosition={setPosition} to="/solutions">
+        Solutions
+      </NavTab>
+      <NavTab setPosition={setPosition} to="/faq">
+        FAQs
+      </NavTab>
 
       <Cursor position={position} />
     </ul>
   );
 }
 
-const Tab = ({
+const NavTab = ({
   children,
   setPosition,
   className,
+  to,
 }: {
   children: React.ReactNode;
   setPosition: (position: Position) => void;
   className?: ClassNameValue;
+  to: LinkProps["to"];
 }) => {
   const ref = useRef<HTMLLIElement>(null);
   return (
-    <li
-      ref={ref}
-      onMouseEnter={() => {
-        if (!ref.current) return;
+    <Link to={to}>
+      <li
+        ref={ref}
+        onMouseEnter={() => {
+          if (!ref.current) return;
 
-        const { width } = ref.current.getBoundingClientRect();
-        setPosition({
-          width,
-          opacity: 1,
-          left: ref.current.offsetLeft,
-        });
-      }}
-      className={cn(
-        "relative z-10 block cursor-pointer px-3 py-1.5 -translate-y-0.5 text-base! md:text-base rounded-full hover:bg-secondary",
-        className,
-      )}
-    >
-      {children}
-    </li>
+          const { width } = ref.current.getBoundingClientRect();
+          setPosition({
+            width,
+            opacity: 1,
+            left: ref.current.offsetLeft,
+          });
+        }}
+        className={cn(
+          "relative z-10 block cursor-pointer px-3 py-1.5 text-base! md:text-base rounded-full hover:bg-secondary",
+          className,
+        )}
+      >
+        {children}
+      </li>
+    </Link>
   );
 };
 
@@ -80,7 +84,7 @@ const Cursor = ({ position }: { position: Position }) => {
   return (
     <motion.li
       animate={position}
-      className="absolute z-0 h-9 -translate-y-1.5 max-sm:-translate-y-1 rounded-full bg-secondary md:h-10"
+      className="absolute z-0 h-9 -translate-y-2 max-sm:-translate-y-2 rounded-full bg-secondary"
     />
   );
 };
